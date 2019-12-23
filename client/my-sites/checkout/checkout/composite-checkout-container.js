340c416d9795118cefec69c095233744676072a7
Payton Swick <payton@foolord.com>
2019-12-23 17:56:21 -0500

    Remove tax from items and add country

--- client/my-sites/checkout/checkout/composite-checkout-container.js
+++ client/my-sites/checkout/checkout/composite-checkout-container.js
@@ -101,9 +101,10 @@ function formatDataForPayPalExpressEndpoint( {
 			subdivisionCode,
 			phoneNumber,
 			couponId,
-			items,
+			items: items.filter( item => item.type !== 'tax' ),
 		} ),
 		domainDetails,
+		country,
 		postalCode,
 	};
 }
